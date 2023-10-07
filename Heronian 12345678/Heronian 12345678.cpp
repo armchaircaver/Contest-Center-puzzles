413a9f6cfdf3fc2b7d6d4a9131d77d943867cd4d
Arthur's Laptop <arthur.vause@gmail.com>
2023-10-07 13:49:22 +0100

    Use arrays, time results, experiment with performance fixes

--- Heronian 12345678/Heronian 12345678.cpp
+++ Heronian 12345678/Heronian 12345678.cpp
@@ -7,6 +7,8 @@
 #include <fstream>
 #include <iostream>
 #include <set>
+#include <time.h>
+
 
 class CandidateTwosquares {
 public:
@@ -27,23 +29,27 @@ public:
 
 CandidateTwosquares cts;
 
-//std::vector <std::vector<int64_t>>T;
-int64_t T[100000];
 int ti = 0;
 
-//typedef struct ipair { int64_t x[2]; } ipair;
-//int64_t Ts[100000][2];
+std::array<std::array<int64_t, 2>,100000> Tset;
 
+void print_time() {
+	time_t my_time = time(NULL);
+	char str[26];
+	ctime_s(str, sizeof str, &my_time);
+	printf("%s \n", str);
+}
 
 int main() {
 
+	print_time();
 
 	for (auto A : cts.cand_twosquares) {
 		int64_t a = stoi(A.first);
 		//if (a != 15487236) continue;
 		//std::cout << "a=" << a << "\n";
 
-		for (auto &B : cts.cand_twosquares) {
+		for (auto& B : cts.cand_twosquares) {
 			int64_t b = stoi(B.first);
 			if (b <= a)
 				continue;
@@ -52,32 +58,31 @@ int main() {
 			ti = 0;
 			for (auto& wx : A.second)
 				for (auto& yz : B.second) {
-					//T.push_back({ wx[0] * yz[0] + wx[1] * yz[1], abs(wx[0] * yz[1] - wx[1] * yz[0]) });
-					//int64_t p = wx[0] * yz[0] + wx[1] * yz[1];
-					//int64_t q = abs(wx[0] * yz[1] - wx[1] * yz[0]);
-					T[ti++] = wx[0] * yz[0] + wx[1] * yz[1];
-					T[ti++] = abs(wx[0] * yz[1] - wx[1] * yz[0]);
-				
-					//Ts[0][0] = p < q ? p : q;  // sort each pair
-					//Ts[0][1] =  p < q ? q : p ;  // sort each pair
 
+					int64_t p = wx[0] * yz[0] + wx[1] * yz[1];
+					int64_t q = abs(wx[0] * yz[1] - wx[1] * yz[0]);
+					Tset[ti++] = { p < q ? p : q ,p < q ? q : p };
 
-					T[ti++] = wx[0] * yz[1] + wx[1] * yz[0];
-					T[ti++] = abs(wx[0] * yz[0] - wx[1] * yz[1]);
-					//T.push_back({ wx[0] * yz[1] + wx[1] * yz[0], abs(wx[0] * yz[0] - wx[1] * yz[1]) });
+					p = wx[0] * yz[1] + wx[1] * yz[0];
+					q = abs(wx[0] * yz[0] - wx[1] * yz[1]);
+					Tset[ti++] = { p < q ? p : q ,p < q ? q : p };
 				}
-			//for (int i = 0; i < ti; i++) std::cout << T[i] << ", ";
-			//std::cout << "\n";
 
-			//std::cout << "a,b = " << a << "," << b << " ,"  "\n";
-			//for (auto xy : T)
-			//	std::cout << "   " << xy[0] << " " << xy[1] << "\n";
+			//std::sort(Tset.begin(), Tset.begin() + ti);
 
-			//for (auto &xy : T) {
-			for (int t = 0; t < ti; t += 2)
+			for (int t = 0; t < ti; t++) {
+				//if (t > 0 && Tset[t - 1] == Tset[t]) 	continue;
 				for (auto i : { 0,1 }) {
-					int64_t d = T[t + i];
-					int64_t Area = T[t+ 1 - i] / 2;
+
+
+					int64_t d = Tset[t][i];
+					if (Tset[t][1 - i] % 2 > 0) 
+						continue;
+					int64_t Area = Tset[t][1 - i] / 2;
+					
+					// look at https://stackoverflow.com/questions/4351371/c-performance-challenge-integer-to-stdstring-conversion
+										
+
 					//if (Area == 4714211251386ull) std::cout << Area << " " << d << " (solution)\n";
 
 					//std::cout << "d=" << d << "\n";
@@ -88,27 +93,43 @@ int main() {
 					//std::cout << "c2 (-2d) = " << c2 << ", c=" << sqrt(c2) << "\n";
 					if (c2 > b * b && is_square(c2)) {
 						int64_t c = (int64_t)sqrt((long double)c2);
-						int64_t Area = T[t + 1 - i] / 2;
 						if (cts.cand_twosquares.find(std::to_string(c)) != cts.cand_twosquares.end()) {
 							std::string Area_st = std::to_string(Area);
-							if ( Area_st.find('0') == std::string::npos && Area_st.find('9') == std::string::npos)
-							  std::cout << "SOLUTION " << a << " " << b << " " << c << " " << Area << "\n";
+							if (Area_st.find('0') == std::string::npos && Area_st.find('9') == std::string::npos) {
+								std::cout << "SOLUTION " << a << " " << b << " " << c << " " << Area << "\n";
+								print_time();
+							}
 						}
 					}
 					c2 = a * a + b * b + 2 * d;
 					//std::cout << "c2 (+2d) = " << c2 << ", c=" << sqrt(c2) << "\n";
 					if (is_square(c2)) {
 						int64_t c = (int64_t)sqrt((long double)c2);
-						int64_t Area = T[t + 1 - i] / 2;
 						if (cts.cand_twosquares.find(std::to_string(c)) != cts.cand_twosquares.end()) {
 							std::string Area_st = std::to_string(Area);
-							if (Area_st.find('0') == std::string::npos && Area_st.find('9') == std::string::npos)
+							if (Area_st.find('0') == std::string::npos && Area_st.find('9') == std::string::npos) {
 								std::cout << "SOLUTION " << a << " " << b << " " << c << " " << Area << "\n";
+								print_time();
+							}
 						}
 					}
 
 				}
+			}
+
 		}
-		
 	}
+	std::cout << "end time : ";
+	print_time();
 }
+
+/* 
+Performance tuning attempts:
+Time to first solution
+without any mods:							2.3 min
+(time to completion 22.5 min)
+with string check to eliminate '0'& '9' :	6.5 min
+with sprintf check to eliminate '0'& '9' :	>20 min
+with sorting and eliminating duplicates :	3.5 min
+
+*/
